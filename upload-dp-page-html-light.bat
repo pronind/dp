18d0402c3888d54ae7cc9863d5ff55f78ce9061a
dpronin <tonicpochta@ya.ru>
2021-03-15 12:14:17 +0800

    микро-правки по шрифтам

--- upload-dp-page-html-light.bat
+++ upload-dp-page-html-light.bat
@@ -3,7 +3,7 @@
 "C:\Program Files (x86)\WinSCP\WinSCP.com" ^
   /command ^
     "open ftp://t13441:q3C1cWkl@hostde3.fornex.org/" ^
-    "lcd D:\YandexDisk\Work\d-page\d-page-html-light" ^
+    "lcd D:\YandexDisk\Work\dp" ^
     "cd /public_html/dpronin.com" ^
     "put -neweronly *" ^
     "exit"
