7bf28264dbe03a264eeb62635f5ed1da034ed70a
dpronin <tonicpochta@ya.ru>
2021-03-30 16:01:59 +0800

    suisse font integration

--- upload-dp-page-html-light.bat
+++ upload-dp-page-html-light.bat
@@ -5,7 +5,7 @@
     "open ftp://t13441:q3C1cWkl@hostde3.fornex.org/" ^
     "lcd D:\YandexDisk\Work\dp" ^
     "cd /public_html/dpronin.com" ^
-    "put -neweronly *" ^
+    "put -neweronly -rawtransfersettings[1] ExcludeHiddenFiles=1 *" ^
     "exit"
 
 set WINSCP_RESULT=%ERRORLEVEL%
